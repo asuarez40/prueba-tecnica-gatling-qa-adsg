b9349965f8fd701df367311c49230080b476cc53
asuarg27 <asuar27@bancodebogota.com.co>
2025-07-20 22:05:57 -0500

    :rocket: Proyecto Inicial

---
+++ src/test/scala/simulation/ConsultaMasiva.scala
@@ -0,0 +1,50 @@
+package simulation
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+import scala.concurrent.duration.DurationInt
+
+class ConsultaMasiva extends Simulation{
+  val applicaion: String = "application/json"
+  val httpConfig = http.baseUrl("https://reqres.in/api")
+    .acceptHeader(applicaion)
+    .contentTypeHeader(applicaion)
+
+  val scenario1 = scenario("Test consulta masiva de usuarios")
+    .repeat(1) {
+      exec(
+        http("Api get all users")
+          .get("/users?page=2")
+          .header("x-api-key", "reqres-free-v1")
+          .check(
+            status.is(200),
+            responseTimeInMillis.lte(500),
+            jsonPath("$.data").exists,
+            jsonPath("$.data").find.saveAs("jsonData"),
+            jsonPath("$.data[0].email").exists
+          )
+          .check(bodyString.saveAs("response"))
+      )
+        .exec {
+            session => {
+              println(session("response").as[String])
+              val data = session("jsonData").asOption[String]
+              println(s" JSON recibido: ${data.getOrElse("NULO")}")
+              println(session("jsonData").asOption[String].getOrElse("NULO"))
+              session
+            }
+        }
+    }
+
+  setUp(
+    scenario1.inject(
+      atOnceUsers(2)
+      //constantConcurrentUsers(10).during(10.seconds)
+      //      rampUsers(10).during(2),
+      //      constantUsersPerSec(2).during(2.seconds),
+      //      constantUsersPerSec(1).during(5).randomized,
+    ).protocols(httpConfig))
+
+
+}
---
+++ src/test/scala/simulation/RegistroUsuario.scala
@@ -0,0 +1,41 @@
+package simulation
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+import scala.concurrent.duration.DurationInt
+
+class RegistroUsuario extends Simulation{
+  val applicaion: String = "application/json"
+  val httpConfig = http.baseUrl("https://reqres.in/api")
+    .acceptHeader(applicaion)
+    .contentTypeHeader(applicaion)
+
+  val scenario1 = scenario("Test registros de usuarios")
+    .repeat(1) {
+      exec(
+        http("Api Create user")
+          .post("/users")
+          .header("x-api-key","reqres-free-v1")
+          .body(RawFileBody(filePath = "data/bodyApiRegistroUsuario.json")).asJson
+          .check(
+            status.is(201),
+            responseTimeInMillis.lte(500)
+          )
+          .check(bodyString.saveAs("response"))
+          )
+        .exec{
+          session => println(session("response").as[String]); session
+        }
+    }
+
+  setUp(
+    scenario1.inject(
+      //atOnceUsers(2)
+        rampUsers(100).during(20.seconds)
+//      rampUsers(10).during(2),
+//      constantUsersPerSec(2).during(2.seconds),
+//      constantUsersPerSec(1).during(5).randomized,
+    ).protocols(httpConfig))
+
+}
